5d6d35ffa3abe2e226136500b881a4af9bf7941e
unverifiedchaos <saifullahkhan@cmr.ac.in>
2022-01-03 16:46:31 +0530

    made input commands, yet to make them work

--- main.c
+++ main.c
@@ -24,13 +24,14 @@ typedef enum {
 //enum and struct for checking commands
 typedef enum {
     STATEMENT_INSERT,
-    STATEMENT_SELECT
+    STATEMENT_SELECT,
 } StatementType;
 
 typedef struct {
     StatementType type;
 } Statement;
 
+//declaring buffer
 InputBuffer* new_input_buffer() { 
     InputBuffer* input_buffer = (InputBuffer* )malloc(sizeof(InputBuffer)); input_buffer->buffer = NULL;
     input_buffer->buffer_length = 0;
@@ -46,6 +47,20 @@ MetaCommandResult do_meta_command (InputBuffer* input_buffer) {
     } else {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
     }
+}
+
+//used in execute_statement, returns state of the input instruction
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if(strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if(strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_COMMAND;
 }
 
 //function to determine command
@@ -70,7 +85,17 @@ void close_input_buffer (InputBuffer* input_buffer) {
     free(input_buffer);
 }
 
-
+//fetches result from PrepareResult
+void execute_statement(Statement* statement) {
+    switch (statement->type) {
+        case(STATEMENT_INSERT):
+            printf("imma do insert here boi!\n");
+            break;
+        case(STATEMENT_SELECT):
+            printf("imma do select here boi!\n");
+            break;
+    }
+}
 
 int main(int argc, char* argv[]) {
    InputBuffer* input_buffer = new_input_buffer();
@@ -81,11 +106,21 @@ int main(int argc, char* argv[]) {
        if(input_buffer->buffer[0] == '.') {
            switch (do_meta_command(input_buffer)) {
                case(META_COMMAND_SUCCESS):
-                   continue;
+                   continue; //by default we break a case statement but since we need to continue our loop we use continue
                case(META_COMMAND_UNRECOGNIZED_COMMAND):
                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
                    continue;
            }
        }
+       Statement statement;
+       switch(prepare_statement(input_buffer, &statement)) {
+           case(PREPARE_SUCCESS):
+               break;
+           case(PREPARE_UNRECOGNIZED_COMMAND):
+               printf("Unrecognized command '%s'\n", input_buffer->buffer);
+               continue;
+       }
+       execute_statement(&statement);
+       printf("Executed!\n");
    }
 }
